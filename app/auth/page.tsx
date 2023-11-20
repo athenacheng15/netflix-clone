fb6e635d7e0c787abf10096019072853f8f07cca
yuchuncheng <yuchuncheng@nine-yi.com>
2023-11-20 18:10:55 +0800

    feat (@app/auth): add register to page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import Input from '../components/Input';
 import { useCallback, useState } from 'react';
+import axios from 'axios';
 
 const Auth = () => {
     const [email, setEmail] = useState<string>('');
@@ -16,6 +17,14 @@ const Auth = () => {
             currentVariant === 'login' ? 'register' : 'login',
         );
     }, []);
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', { email, name, password });
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, name, password]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -64,7 +73,10 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button
+                            onClick={register}
+                            className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+                        >
                             {variant === 'login' ? 'Log in' : 'Sign up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
